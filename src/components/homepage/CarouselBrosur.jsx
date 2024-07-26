3083918b691b1baee5d28393b9366d9de5c7823d
rafai123 <rafaimhd123@gmail.com>
2024-07-26 12:10:09 +0700

    fix brosur carousel

--- src/components/homepage/CarouselBrosur.jsx
+++ src/components/homepage/CarouselBrosur.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import brosur1 from "../../assets/img/brosur1.jpeg";
+import brosur2 from "../../assets/img/brosur2.jpeg";
 
 const captionContainerStyle = {
   position: 'relative',
@@ -26,32 +27,10 @@ const CarouselBrosur = () => {
     <Carousel data-bs-theme="dark" className='h-75' >
       <Carousel.Item>
         <img src={brosur1} className='w-100' alt="First slide" />
-        {/* <div style={captionContainerStyle}>
-          <Carousel.Caption style={captionStyle}>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </div> */}
       </Carousel.Item>
       <Carousel.Item>
-        <img src={brosur1} className='w-100' alt="First slide" />
-        {/* <div style={captionContainerStyle}>
-          <Carousel.Caption style={captionStyle}>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </div> */}
+        <img src={brosur2} className='w-100' alt="First slide" />
       </Carousel.Item>
-      <Carousel.Item>
-        <img src={brosur1} className='w-100' alt="First slide" />
-        {/* <div style={captionContainerStyle}>
-          <Carousel.Caption style={captionStyle}>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </div> */}
-      </Carousel.Item>
-      
     </Carousel>
   );
 };
