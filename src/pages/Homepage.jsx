48305b099902273690b221a4e9823649c0af0dbc
rafai123 <rafaimhd123@gmail.com>
2024-07-26 12:08:44 +0700

    fix landing page and remove unused code

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,109 +3,24 @@ import axios from "axios";
 import "./Homepage.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import heroImage from "../assets/svg/hero_landing_pmb.svg";
-import literasiKita from "../assets/svg/LiterasiKita-image.svg";
-import programImage1 from "../assets/svg/program-image1.svg";
-import programImage2 from "../assets/svg/program-image2.svg";
-import donasiImage from "../assets/svg/donasi-image.svg";
-import user1 from "../assets/svg/user-1.svg";
-import user2 from "../assets/svg/user-2.svg";
-import user3 from "../assets/svg/user-3.svg";
-import cardDonasi1 from "../assets/svg/cardDonasi1.svg";
-import cardDonasi2 from "../assets/svg/cardDonasi2.svg";
-import cardDonasi3 from "../assets/svg/cardDonasi3.svg";
-import goldMedal from "../assets/svg/goldMedal.svg";
 import { Link } from "react-router-dom";
 import BoxProgramStudi from "../components/homepage/BoxProgramstudi";
 import CarouselBrosur from "../components/homepage/CarouselBrosur";
 import HeroSection from "../components/homepage/HeroSection";
 
 export default function Homepage() {
-  const [totalDonasiUang, setTotalDonasiUang] = useState(0);
-  const [topDonasiBuku, setTopDonasiBuku] = useState([]);
-  const [topDonasiVideo, setTopDonasiVideo] = useState([]);
-  const [topDonasiUang, setTopDonasiUang] = useState([]);
-  const [topAllDonasi, setTopAllDonasi] = useState([]);
 
   useEffect(() => {
     AOS.init();
     AOS.refresh();
   }, []);
 
-  const getTotalDonasiUang = async () => {
-    try {
-      const response = await axios.get(
-        `${import.meta.env.VITE_APP_LINK_API}/donasi/all-donasi-uang`
-      );
-      setTotalDonasiUang(response.data[0]?.total_nominal_donasi_uang || 0);
-    } catch (error) {
-      console.error("Error fetching total donasi uang:", error.message);
-    }
-  };
-
-  const getTopDonasiBuku = async () => {
-    try {
-      const response = await axios.get(
-        `${import.meta.env.VITE_APP_LINK_API}/donasi/top-donasi-buku`
-      );
-      setTopDonasiBuku(response.data);
-    } catch (error) {
-      console.error("Error fetching top donasi buku:", error.message);
-    }
-  };
-  const getTopDonasiVideo = async () => {
-    try {
-      const response = await axios.get(
-        `${import.meta.env.VITE_APP_LINK_API}/donasi/top-donasi-videos`
-      );
-      setTopDonasiVideo(response.data);
-    } catch (error) {
-      console.error("Error fetching top donasi buku:", error.message);
-    }
-  };
-  const getTopDonasiUang = async () => {
-    try {
-      const response = await axios.get(
-        `${import.meta.env.VITE_APP_LINK_API}/donasi/top-donasi-uang`
-      );
-      setTopDonasiUang(response.data);
-    } catch (error) {
-      console.error("Error fetching top donasi buku:", error.message);
-    }
-  };
-
-  const getTopAllDonasi = async () => {
-    try {
-      const response = await axios.get(
-        `${import.meta.env.VITE_APP_LINK_API}/donasi/top-all-donasi`
-      );
-      setTopAllDonasi(response.data);
-    } catch (error) {
-      console.error("Error fetching top all donasi:", error.message);
-    }
-  };
-
-  useEffect(() => {
-    getTotalDonasiUang();
-    getTopDonasiBuku();
-    getTopDonasiVideo();
-    getTopDonasiUang();
-    getTopAllDonasi();
-  }, []);
-
-  const formatToRupiah = (amount) => {
-    return new Intl.NumberFormat("id-ID", {
-      style: "currency",
-      currency: "IDR",
-    }).format(amount);
-  };
-
   return (
     <>
       <div className="homepage">
         <HeroSection />
 
-        <section id="LiterasiKita" className="mt-lg-5 mb-lg-5">
+        <section id="Prodi" className="mt-lg-5 mb-lg-5">
           <div className="container about-content">
             <br />
             <br />
